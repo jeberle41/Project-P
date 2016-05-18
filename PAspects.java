cb3a4f2881890750c0a8f1fc44bdbc7c6917b1e4
jeberle41 <sharpfan101@gmail.com>
2016-05-17 20:36:58 -0500

    Create PAspects.java

---
+++ PAspects.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+public interface PAspects
+{
+    public String getType();
+    public String getName();
+    public ArrayList<Integer> getStats();
+    public ArrayList<Attack> getMoveset();
+}
